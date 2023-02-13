b4a2277d31dbe80297d4c7cf9b0f171092c4cdfc
Nikita-str <newmynamespace@gmail.com>
2023-02-13 09:51:10 +0300

    parallel page loading/ref extracting

--- src/links.rs
+++ src/links.rs
@@ -1,3 +1,4 @@
+use futures::StreamExt;
 use reqwest::Url;
 use std::collections::HashSet;
 use std::io::Write;
@@ -6,12 +7,18 @@ use std::str::FromStr;
 use select::document::Document;
 use select::predicate::Name;
 
+use std::sync::{Arc, Mutex};
+
+#[derive(Clone)]
 pub struct Links {
-    all: HashSet<Url>,
-    new: Vec<Url>,
+    all: Arc<Mutex<HashSet<Url>>>,
+    new: Arc<Mutex<Vec<Url>>>,
 }
 
 impl Links {
+    /// # panic
+    /// * if `init_links` is empty
+    /// * if any links from `init_links` is bad url
     pub fn new<'x>(init_links: impl IntoIterator<Item = &'x str>) -> Self {
         let all = HashSet::from_iter(init_links.into_iter().map(
             |link|Url::from_str(link).unwrap_or_else(|e|panic!("bad URL({e}): {link}"))
@@ -21,8 +28,8 @@ impl Links {
         let new = Vec::from_iter(all.iter().map(|x|x.clone()));
 
         Self {
-            all,
-            new,
+            all: Arc::new(Mutex::new(all)),
+            new: Arc::new(Mutex::new(new)),
         }
     }
 
@@ -30,8 +37,12 @@ impl Links {
     /// * `Ok(true)` when there still exist unchecked links
     /// * `Ok(false)` when nothing more to check
     /// * `Err(..)` when an error occurs
-    pub async fn next(&mut self) -> anyhow::Result<bool> {
-        let Some(link) = self.new.pop() else { return Ok(false) };
+    pub async fn next(self) -> anyhow::Result<bool> {
+        let link = { // to drop MutexGuard
+            let Ok(mut new) = self.new.lock() else { anyhow::bail!("`Mutex` was poisoned") };
+            let Some(link) = new.pop() else { return Ok(false) };
+            link
+        };
 
         let body = reqwest::get(link.as_str())
             .await?
@@ -43,48 +54,86 @@ impl Links {
             .find(Name("a"))
             .filter_map(|node| node.attr("href"));
 
-        for potential_new_link in body_links {
-            let url = reqwest::Url::from_str(link.as_str())?;
+        { // to drop MutexGuard
+            let Ok(mut new) = self.new.lock() else { anyhow::bail!("`Mutex` was poisoned") };
+            let Ok(mut all) = self.all.lock() else { anyhow::bail!("`Mutex` was poisoned") };
 
-            let potential_new_link = if potential_new_link.contains("://") {
-                reqwest::Url::from_str(potential_new_link)?
-            } else {
-                url.join(potential_new_link)?
-            };
+            for potential_new_link in body_links {
+                let url = reqwest::Url::from_str(link.as_str())?;
 
-            if !self.all.contains(&potential_new_link) {
-                let new_link = potential_new_link;
-                self.all.insert(new_link.clone());
-                self.new.push(new_link);
+                let potential_new_link = if potential_new_link.contains("://") {
+                    reqwest::Url::from_str(potential_new_link)?
+                } else {
+                    url.join(potential_new_link)?
+                };
+
+                if !all.contains(&potential_new_link) {
+                    let new_link = potential_new_link;
+                    all.insert(new_link.clone());
+                    new.push(new_link);
+                }
             }
         }
       
         Ok(true)
     }
 
-    pub async fn take_all_unique(&mut self, print: bool) -> anyhow::Result<()> {
+
+    pub async fn take_all_unique(&mut self, max_together: Option<usize>, print: bool) -> anyhow::Result<()> {
+        let max_together = max_together.map(|x|usize::max(1, x));
+
+        let mut cur_run = 0;
+        let mut total_run = 0;
+
+        let mut awaited = futures::stream::FuturesUnordered::new();
+
         loop {
-            if print { 
-                print!("+");
-                let _ = std::io::stdout().flush();
+            'awaiter_filling: loop {
+                let can_add = max_together.map(|max|cur_run < max).unwrap_or(true);
+                if !can_add { break 'awaiter_filling }
+                
+                if total_run < self.len() {
+                    cur_run += 1;
+                    total_run += 1;
+                    awaited.push(self.clone().next());
+                } else {
+                    break 'awaiter_filling
+                }
             }
 
-            if !self.next().await? {
-                if print { println!() }
-                return Ok(())
+            // `if awaited.is_empty() {..}` is the same case as
+            //             `awaited.next().await` return `None`
+
+            match awaited.next().await {
+                Some(result) => {
+                    cur_run -= 1;
+                    let _ = result?;            
+                    if print {
+                        print!("+");
+                        std::io::stdout().flush()?;
+                    }
+                }
+                None => {
+                    if print { println!() }
+                    return Ok(())
+                }
             }
         }
     }
 
     pub fn len(&self) -> usize {
-        self.all.len()
+        match self.all.lock() {
+            Ok(all) => all.len(),
+            _ => 0, // not sure
+        }
     }
 }
 
 impl std::fmt::Display for Links {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "[{}]:", self.len())?;
-        for link in &self.all {
+        let Ok(all) = self.all.lock() else { return Ok(()) };
+        for link in all.iter() {
             writeln!(f, "{link}")?
         }
         Ok(())
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,16 @@ use site_link_extract::Links;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let mut links = Links::new(vec!["https://www.superbad.com"]);
-    links.take_all_unique(true).await?;
+    
+    // // load link by link (slow):  
+    // links.take_all_unique(Some(1), true).await?;
+
+    // // load scoped but no more 8 at the same time:
+    // links.take_all_unique(Some(8), true).await?;
+    
+    // load scoped and without restrictions:
+    links.take_all_unique(None, true).await?;
+    
     println!("{links}");
     Ok(())
 }
